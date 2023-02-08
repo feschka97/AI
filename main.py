8dbb6b47673436ede4c8e3db3d28125ffa5cc259
feschka97 <53889942+feschka97@users.noreply.github.com>
2023-02-08 12:14:50 +0700

    Add files via upload

--- main.py
+++ main.py
@@ -9,18 +9,36 @@ def deone():
     else:
         print("Таковой буквы нема")
 def detwo():
-    lim = input("Введите n:")
+    lim = int(input("Введите n дляя предельного числа в последовательности Фибоначчи:"))
     curr=1
     next=1
     while next < lim:
         curr +=next
         next +=curr
     print(curr)
+def dethree():
+    fact = int(input("Введите n для факториала:"))
+    start = 1
+    output = 1
+    while start <= fact:
+        output *=start
+        start+=1
+    print(output)
+def defour():
+    binary = int(input("Введите число:"))
+    output=list()
+    if binary % 2:
+        output.append(1)
+    else:
+        output.append(0)
+    print (output)
+
 
 choice = 1
-choice = input("Задание")
+choice = input("Задание: \n")
 match choice:
-    case 1: deone()
-    case 2: detwo()
-    case 3: dethree()
+    case "1": deone()
+    case "2": detwo()
+    case "3": dethree()
+    case "4": defour()
     case _: print("Паника!")
