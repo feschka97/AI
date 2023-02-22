256da95bf586f9f0664bd750da909642075f1075
feschka97 <53889942+feschka97@users.noreply.github.com>
2023-02-22 14:46:28 +0700

    Update lr2.py

--- lr2.py
+++ lr2.py
@@ -1,32 +1,37 @@
 import random
 
-
 def deone():
     print(sum(listin))
+
 def detwo():
     print(max(listin))
+
 def dethree():
     var=0
     for var in listin:
         if listin.count(var) !=0:
             print(var," - дубликат")
+
 def defour():
     min = min(listin)
     max= max(listin)
     listin[listin.index(min)] = max
     listin[listin.index(max)] = min
+
 def defah():
     frommax = 0
     frommax_team=[]
     frommax_team.append(max(listin))
-    current = listin.index(random.randint(0, len(listin)))
-    while max(frommax_team)+frommax_team[len(frommax_team)] < current:
+    current = listin[:-1]
+    lower =0
+    while max(frommax_team)+lower < current:
         frommax_team.append(current)
         frommax_team.sort()
         frommax=frommax_team[0]+frommax_team[1]
         listin.remove(current)
         print("Добавили в игрока с ПП ", current)
         current = listin.index(random.randint(0,listin.count()))
+        lower=frommax_team[:-2]
         print("Текущее ПП в команде: ", sum(frommax_team))
 
 
@@ -35,8 +40,8 @@ def defah():
 try:
     rook=0
     listin = []
-    while rook != random.randint(0,100):
-        listin.append((random.randint(0,100)))
+    while rook != random.randint(0,5):
+        listin.append((random.randint(0,5)))
     choice = 1
     choice = input("Задание: \n")
     match choice:
