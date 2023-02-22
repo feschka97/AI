d5dd172115e0686618ab9cd0fc39d48e63258f6e
feschka97 <53889942+feschka97@users.noreply.github.com>
2023-02-22 14:13:09 +0700

    Update lr2.py

--- lr2.py
+++ lr2.py
@@ -16,9 +16,22 @@ def defour():
     listin[listin.index(min)] = max
     listin[listin.index(max)] = min
 def defah():
-    
-    
-        
+    frommax = 0
+    frommax_team=[]
+    frommax_team.append(max(listin))
+    current = listin.index(random.randint(0, len(listin)))
+    while max(frommax_team)+frommax_team[len(frommax_team)] < current:
+        frommax_team.append(current)
+        frommax_team.sort()
+        frommax=frommax_team[0]+frommax_team[1]
+        listin.remove(current)
+        print("Добавили в игрока с ПП ", current)
+        current = listin.index(random.randint(0,listin.count()))
+        print("Текущее ПП в команде: ", sum(frommax_team))
+
+
+
+
 try:
     rook=0
     listin = []
