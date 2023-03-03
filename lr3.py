169c6c190eb82e80da33ad13f02db7805d0ab144
feschka97 <53889942+feschka97@users.noreply.github.com>
2023-03-03 09:28:05 +0700

    Update lr3.py

--- lr3.py
+++ lr3.py
@@ -1,17 +1,14 @@
 import math
-import string
-
 
 def func(x,y):
-    func1 = round(pow(math.e,(math.log(math.e,4*20)))+pow(x,3)-5*y)
-    func2 = round(math.sqrt((pow(8,x)-y*y)*math.cos(math.pi/3)) / math.sin(math.pi/6))
-    return "Значения функций",func1,func2
-
-
-
-
+    func1 = pow(math.e,(math.log(math.e,4*20)))+pow(x,3)-5*y
+    func2 = math.sqrt((pow(8,x)-y*y)*math.cos(math.pi/3)) / math.sin(math.pi/6)
+    return "Значения функций:",func1,func2, "Округлённые значения функции:",round(func1),round(func2)
 
 
 
 f = func(int(input("Введите x ")),int(input("Введите y ")))
 print (f)
+
+
+
