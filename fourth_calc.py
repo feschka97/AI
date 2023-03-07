9bd2b1e58ce3d2bed4fcd8025a6d601bd1b41213
feschka97 <53889942+feschka97@users.noreply.github.com>
2023-03-07 10:10:40 +0700

    Update fourth_calc.py

--- fourth_calc.py
+++ fourth_calc.py
@@ -1,12 +1,24 @@
-import math
+PI = 3.141592653589793
 
 
 def sine(x):
-    return x - pow(x, 3) / factorial(3) + pow(x, 5) / factorial(5) - pow(x, 7) / factorial(7)
+    match x:
+        case "x <= 1 && x >= -1":
+            return x - power(x, 3) / 6 + pow(x, 5) / 120 - power(x, 7) / 5040
+        case "x <=2 && x >=-2":
+            return x - power(x, 3) / 6 + pow(x, 5) / 120 - power(x, 7) / 5040 + power(x, 9) / 362880
+        case "x <=3 && x >=-3":
+            return x - power(x, 3) / 6 + pow(x, 5) / 120 - power(x, 7) / 5040 + power(x, 9) / 362880 - power(x,11) / factorial(11)
+        case "x <=6 && x >=-6":
+            return x - power(x, 3) / 6 + pow(x, 5) / 120 - power(x, 7) / 5040 + power(x, 9) / 362880 - power(x,11) / factorial(11) + power(x, 13) / factorial(13)
 
 
 def cosine(x):
-    return 1 - pow(x, 2) / 2 + pow(x, 4) / math.factorial(4) - pow(x, 6) / math.factorial(6)
+    match x:
+        case "x <= 1 && x >= 1":
+            return 1 - power(x, 2) / 2 + power(x, 4) / factorial(4) - power(x, 6) / factorial(6)
+        case "":
+            1 - power(x, 2) / 2 + power(x, 4) / factorial(4) - power(x, 6) / factorial(6) + power(x, 8) / factorial(8)
 
 
 def remainder(x, y):
@@ -33,8 +45,18 @@ def power(base, exp):
 
 
 def tangent(x):
-    return x + power(x, 3)/3 + 2/power(15, 5)*power(x, 5)
+    if mod(x) < PI / 2:
+        return x + power(x, 3) / 3 + 2 / power(15, 5) * power(x, 5)
+    else:
+        return "NaN"
 
 
 def cotangent(x):
-    return 1/x - x/3 - power(x, 3)/45
+    return 1 / x - x / 3 - power(x, 3) / 45
+
+
+def mod(x):
+    if x < 0:
+        return -x
+    else:
+        return x
