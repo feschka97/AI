08a4fcb50dfe25cd8741eb679fa2a31fcb183c67
feschka97 <53889942+feschka97@users.noreply.github.com>
2023-03-14 16:04:54 +0700

    Update lr5.py

--- lr5.py
+++ lr5.py
@@ -6,7 +6,7 @@ try:
     spreadsheet = open("74.csv", "r", errors="ignore")
     for value in spreadsheet:
         if value.isascii:
-            io.write(re.sub("[^0-9,\-\:\]]"))
+            io.write(re.sub("[^0-9][\-\:\]]",'',value))
     spreadsheet.close()
 except Exception as ex:
     print(ex)
