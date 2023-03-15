20b5c4e445c2b26a0f94640dedd0700ca5d57c38
feschka97 <53889942+feschka97@users.noreply.github.com>
2023-03-15 12:11:14 +0700

    Update lr5.py

--- lr5.py
+++ lr5.py
@@ -1,11 +1,12 @@
 import re
-
+import time
+#timer = tim
 io = open("vasya.txt", "w")
 
 try:
     spreadsheet = open("74.csv", "r", errors="ignore")
     for value in spreadsheet:
-        if value.isascii:
+        if time.strptime(value,"%m-%d-%Y "):
             io.write(re.sub("[^0-9][\-\:\]]",'',value))
     spreadsheet.close()
 except Exception as ex:
