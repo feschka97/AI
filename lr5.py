f4bb99ea69ffc763fe1dab83c0c457df379352bc
feschka97 <53889942+feschka97@users.noreply.github.com>
2023-03-17 10:23:02 +0700

    Update lr5.py

--- lr5.py
+++ lr5.py
@@ -1,14 +1,16 @@
+# coding=utf8
 import re
 import time
-#timer = tim
+timer = time.time()
 io = open("vasya.txt", "w")
 
 try:
     spreadsheet = open("74.csv", "r", errors="ignore")
     for value in spreadsheet:
-        if time.strptime(value,"%m-%d-%Y "):
-            io.write(re.sub("[^0-9][\-\:\]]",'',value))
+        if time.strptime(re.sub("[^\d]", '', value),"%m%d%Y%H%M%f"):
+            io.write(re.sub("[^\d\n\-\:\t]", '', value))
     spreadsheet.close()
+    print('Затрачено:',time.time()-timer)
 except Exception as ex:
     print(ex)
 
