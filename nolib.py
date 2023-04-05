04e7923b196dfdd534d75e292fa0ab3463980940
feschka97 <53889942+feschka97@users.noreply.github.com>
2023-04-05 14:43:30 +0700

    Update nolib.py

--- nolib.py
+++ nolib.py
@@ -1,21 +1,40 @@
 def Fixup(str):
-    str = input("Введите переправленный вариант")
-    if str[1,2,4,5,7:10,12,13,15,16,18,19,21:24].isnumeric() and str[3,6]=='-'and str[11,17,20] =='\t' and str[14]=='\:':
-        print("Плохой ввод")
-        Fixup(str)
+    print(str)
+    str = input("Введите переправленный вариант\n")
+    Checkque(str)
+
+def Checkque(str):
+    for i in indeces:
+        if str[i].isdigit():
+            continue
+        else:
+            Fixup(str)
+    for uh in minus:
+        if str[uh] == '-':
+            continue
+        else:
+            Fixup(str)
+    for em in tab:
+        if str[em] == '\t':
+            continue
+        else:
+            Fixup(str)
+    io.write(str)
+
+
 
 try:
     io = open("vasya.txt", "w")
     spreadsheet = open("74.csv", "r", errors="ignore")
     array = spreadsheet.readlines()
-    indeces = [1,2,4,5,range(7,10),12,13,15,16,18,19,range(21,24)]
+    indeces = [1,2,4,5,7,8,9,10,12,13,15,16,18,19,21,22,23]
+    minus = [3,6]
+    tab = [11,17,20]
+    for value in array:
+        Checkque(value)
 
-    for row in array:
-        if [row[i] for i in indeces] and row[3,6]=='-'and row[11,17,20] =='\t' and row[14]=='\:':
-            io.write(row)
-        else:
-            Fixup(row)
-        spreadsheet.close()
+    spreadsheet.close()
 except Exception as ex:
         print(ex)
+
 
