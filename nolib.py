a99b9eab8fdc95ae8e1be3879407262bf20bf36e
feschka97 <53889942+feschka97@users.noreply.github.com>
2023-04-06 23:14:05 +0700

    Update nolib.py

--- nolib.py
+++ nolib.py
@@ -7,20 +7,18 @@ def Checkque(str):
         if str[i].isdigit():
             continue
         else:
-            Fixup(str)
-            break
+            str = Fixup(str)
+
     for uh in minus:
         if str[uh] == '-':
             continue
         else:
-            Fixup(str)
-            break
+            str = Fixup(str)            
     for em in tab:
         if str[em] == '\t':
             continue
         else:
-            Fixup(str)
-            break
+            str = Fixup(str)            
     return str
 try:
     io = open("vasya.txt", "w")
@@ -29,9 +27,8 @@ try:
     indeces = [1,2,4,5,7,8,9,10,12,13,15,16,18,19,21,22,23]
     minus = [3,6]
     tab = [11,17,20]
-    for value in array:
-        Checkque(value)
-        io.write(value)
+    for value in array:        
+        io.write(Checkque(value))
     spreadsheet.close()
 except Exception as ex:
         print(ex)
