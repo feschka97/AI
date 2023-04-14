563a472aca5494e98e0b1f24e6600288dc846d29
feschka97 <53889942+feschka97@users.noreply.github.com>
2023-04-14 09:39:22 +0700

    Update samples.py

--- samples.py
+++ samples.py
@@ -5,16 +5,20 @@ try:
     spreadsheet = open("vasya.txt", "r", errors="ignore")
     spreadsheet = spreadsheet.readlines()
     f = open('test.csv', 'w')
+    cases = {0,1}
+    values1 = [346,395]
+    values2 = [148,99]
     TSample = []
     TrSample = []
-    while len(TSample) != 346: # 494*.7
-        eh = random.randint(0,494)
-        TSample.append(spreadsheet[eh])
-        spreadsheet.pop(eh)
-    while len(TrSample) != 148: #494*.3
-        eh = random.randint(0, 494)
-        TrSample.append(spreadsheet[eh])
-        spreadsheet.pop(eh)
+    for value in cases:
+        while len(TSample) != values1[value]:
+            eh = random.randint(0,494)
+            TSample.append(spreadsheet[eh])
+            spreadsheet.pop(eh)
+        while len(TrSample) != values2[value]:
+            eh = random.randint(0, 494)
+            TrSample.append(spreadsheet[eh])
+            spreadsheet.pop(eh)
 
 
 except Exception as ex:
