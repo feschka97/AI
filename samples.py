67ffc8bd720c0afec5d10d6479a4ba39c986b5c1
feschka97 <53889942+feschka97@users.noreply.github.com>
2023-04-18 10:13:16 +0700

    fin

--- samples.py
+++ samples.py
@@ -2,24 +2,21 @@ import re
 import random
 import pandas as pd
 try:
-    spreadsheet = open("vasya.txt", "r", errors="ignore")
-    spreadsheet = spreadsheet.readlines()
     f = open('test.csv', 'w')
     cases = {0,1}
     values1 = [346,395]
     values2 = [148,99]
     TSample = []
     TrSample = []
-    for value in cases:
-        while len(TSample) != values1[value]:
-            eh = random.randint(0,494)
+    for value in cases: #346/148 - 70%/30%; 395/99 - 80%/20%
+        spreadsheet = open("vasya.txt", "r", errors="ignore")
+        spreadsheet = spreadsheet.readlines()
+        while len(TSample) < values1[value]:
+            eh = random.randint(0,len(spreadsheet)-1)
             TSample.append(spreadsheet[eh])
             spreadsheet.pop(eh)
-        while len(TrSample) != values2[value]:
-            eh = random.randint(0, 494)
-            TrSample.append(spreadsheet[eh])
-            spreadsheet.pop(eh)
-
+        TrSample = spreadsheet.copy()
+        print("жопа")
 
 except Exception as ex:
     print(ex)
