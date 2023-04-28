b0f600da7c78c76447644db75ecc5d96f1d86f2b
feschka97 <53889942+feschka97@users.noreply.github.com>
2023-04-28 09:50:53 +0700

    var1

--- nolib.py
+++ nolib.py
@@ -1,36 +1,36 @@
-def Fixup(str):
-    print(str)
-    str = input("Введите переправленный вариант\n") + "\n"
-    return str
-def Checkque(str):
-    for i in indeces:
-        if str[i].isdigit():
-            continue
-        else:
-            str = Fixup(str)
+class BruhMemento(Exception): pass
 
-    for uh in minus:
-        if str[uh] == '-':
-            continue
-        else:
-            str = Fixup(str)            
-    for em in tab:
-        if str[em] == '\t':
-            continue
-        else:
-            str = Fixup(str)            
-    return str
+def Checkque(stre):
+    try:
+        for i in indeces:
+            if stre[i].isdigit():
+                continue
+            else:
+                raise BruhMemento
+        for uh in minus:
+            if stre[uh] == '-':
+                continue
+            else:
+                raise BruhMemento
+        for em in tab:
+            if stre[em] == '\t':
+                continue
+            else:
+                raise BruhMemento
+        return True
+    except BruhMemento:
+        tablo.write(stre)
 try:
     io = open("vasya.txt", "w")
     spreadsheet = open("74.csv", "r", errors="ignore")
     array = spreadsheet.readlines()
-    indeces = [1,2,4,5,7,8,9,10,12,13,15,16,18,19,21,22,23]
-    minus = [3,6]
-    tab = [11,17,20]
-    for value in array:        
-        io.write(Checkque(value))
+    indeces = [1, 2, 4, 5, 7, 8, 9, 10, 12, 13, 15, 16, 18, 19, 21, 22, 23]
+    minus = [3, 6]
+    tab = [11, 17, 20]
+    tablo = open("errorsbruh.txt", 'w')
+    for value in array:
+        if (Checkque(value)):
+            io.write(value)
     spreadsheet.close()
 except Exception as ex:
-        print(ex)
-
-
+    print(ex)
