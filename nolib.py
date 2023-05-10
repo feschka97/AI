e5ccc8523f897fe70974316b87736befe59eea4e
feschka97 <53889942+feschka97@users.noreply.github.com>
2023-05-10 10:24:57 +0700

    nil

--- nolib.py
+++ nolib.py
@@ -1,36 +1,40 @@
 def Fixup(stre,mistakes):
-    stre = stre.replace(stre[mistakes.index(1)],'')
-    mistakes[mistakes.index(1)] = 0
-    print(stre[22]) # '"12-16-1199	8l:	33	8"'
-                    # '"12-16-1990	08:00	58	205"'
+    stray = ''
+    for indx in range(len(mistakes)):
+        stray+=stre[indx]
+
+
     if (Checkque(stre)):
-        return stre
+        return stray
 
 
 def Checkque(stre):
-    mistakes = [0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0]
     for i in indeces:
         if stre[i].isdigit():
             mistakes[i] = 0
             continue
         else:
             mistakes[i] = 1
+            break
     for uh in minus:
         if stre[uh] == '-':
             mistakes[uh] = 0
             continue
         else:
             mistakes[uh] = 1
+            break
     for em in tab:
         if stre[em] == '\t':
             mistakes[em] = 0
             continue
         else:
             mistakes[em] = 1
+            break
     if mistakes == [0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0]:
         return True
     else:
-        stre = Fixup(stre,mistakes)
+        return False
+
 
 try:
     io = open("vasya.txt", "w")
@@ -40,8 +44,12 @@ try:
     minus = [3, 6]
     tab = [11, 17, 20]
     tablo = open("errorsbruh.txt", 'w')
+    mistakes = [0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0]
     for value in array:
         if (Checkque(value)):
+            io.write(value)
+        else:
+            value = Fixup(value, mistakes)
             io.write(value)
     spreadsheet.close()
 except Exception as ex:
