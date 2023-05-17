2072083064e6d0c9d6e63280aaca76b0f9fca391
feschka97 <53889942+feschka97@users.noreply.github.com>
2023-05-17 16:08:21 +0700

    i guess fin

--- 5.py
+++ 5.py
@@ -4,25 +4,26 @@ spreadsheet = io.readlines()
 mean = 0
 median = 0
 mode = 0
-newb = {}
+counts = []
 length = len(spreadsheet)
+for index in range(length):
+    spreadsheet[index] = int(re.sub("\"\d\d-\d\d-\d\d\d\d\s\d\d:\d\d\t\d\d\t", '', 
+                                    spreadsheet[index]).removesuffix('\"\n'))
 for value in spreadsheet:
-    newb.
-for value in newb:
     mean += value
+    counts.append(spreadsheet.count(value))
 mean /= length
 if length % 2 == 0:
-    median = newb[length//2]
+    median = spreadsheet[length//2]
 else:
     index = int(length/2)
-    median = (newb[index]+newb[index+1])/2
-for loop in length:
-    newb[loop].count()
+    median = (spreadsheet[index]+spreadsheet[index+1])/2
+mode = spreadsheet[(max(counts))]
 
-
+mode = spreadsheet[counts.index(max(counts))]
 
 
 
 print(mean, "- среднее значение")
 print(median, "- медиана")
-print(mode, "-мода")
+print(mode, "- мода, с",max(counts), "вхождениями")
