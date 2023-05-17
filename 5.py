114c46a945548bb576f2e05b89ef541cbea22416
feschka97 <53889942+feschka97@users.noreply.github.com>
2023-05-17 16:38:15 +0700

    wip

--- 5.py
+++ 5.py
@@ -7,18 +7,17 @@ mode = 0
 counts = []
 length = len(spreadsheet)
 for index in range(length):
-    spreadsheet[index] = int(re.sub("\"\d\d-\d\d-\d\d\d\d\s\d\d:\d\d\t\d\d\t", '', 
-                                    spreadsheet[index]).removesuffix('\"\n'))
+    spreadsheet[index] = int(re.sub("\"\d\d-\d\d-\d\d\d\d\s\d\d:\d\d\t\d\d\t", '', spreadsheet[index]).removesuffix('\"\n'))
 for value in spreadsheet:
     mean += value
     counts.append(spreadsheet.count(value))
 mean /= length
+
 if length % 2 == 0:
     median = spreadsheet[length//2]
 else:
     index = int(length/2)
     median = (spreadsheet[index]+spreadsheet[index+1])/2
-mode = spreadsheet[(max(counts))]
 
 mode = spreadsheet[counts.index(max(counts))]
 
@@ -26,4 +25,5 @@ mode = spreadsheet[counts.index(max(counts))]
 
 print(mean, "- среднее значение")
 print(median, "- медиана")
-print(mode, "- мода, с",max(counts), "вхождениями")
+print(mode, "- мода, с ",max(counts))
+#дисперсия
