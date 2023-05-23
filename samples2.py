10113c813333bc15cca5c8c7a41b666932bb8483
feschka97 <53889942+feschka97@users.noreply.github.com>
2023-05-23 12:56:02 +0700

    fin 3

--- samples2.py
+++ samples2.py
@@ -1,18 +1,17 @@
 import random
-try:
-    
-    spreadsheet = open("vasya.txt", "r", errors="ignore")
-    spreadsheet = spreadsheet.readlines()
-    Test =[]
-    while len(Test) < 60: #494 * .12 = 60
-        eh = random.randint(0,len(spreadsheet)-1)
-        Test.append(spreadsheet[eh])
-        spreadsheet.pop(eh)
-    Comp = spreadsheet.copy()
-    Vald = Comp[:int((len(Comp))/7)]
-    Tren = Comp[int((len(Comp)/7)):]
-    print("zhopa")
-
-
-except Exception as ex:
-    print(ex)
+k = 7
+spreadsheet = open("74.csv", "r", errors="ignore")
+spreadsheet = spreadsheet.readlines()
+Test =[]
+oldLen = int(len(spreadsheet)*0.12)
+while len(Test) < oldLen:
+	eh = random.randint(0,len(spreadsheet)-1)
+	Test.append(spreadsheet[eh])
+	spreadsheet.pop(eh) # 12%
+Comp = spreadsheet.copy()
+for podmn in range(1,k+1):
+	split = int((len(Comp))/k)
+	Vald = Comp[split*(podmn-1):split*podmn]
+	Tren = [x for x in Comp if (x not in Vald)]
+	print(f'{podmn}-ая валидаточная часть: {Vald}')
+	print(f'{podmn}-ая тренировочная часть: {Tren}')
